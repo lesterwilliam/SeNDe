d8bc5eb488b7e53768a458f4626111a97362ea01
lesterwilliam <adrian.schwizgebel@gmail.com>
2018-12-12 15:57:56 +0100

    Now prints targets to csv file.

--- sende.py
+++ sende.py
@@ -1,10 +1,11 @@
-# Python code to search for the auto-created folders from the Renesas HEW IDE.
+# Python code designed to search for the auto-created folders from the Renesas HEW IDE.
 # Once detected, choose the 'delete option' to delete all these folders.
 
 import sys
 import os
+import csv
 
-filelist = []
+dirlist = []
 os.path.start_path = 0
 
 def welcome_msg():
@@ -16,19 +17,28 @@ def search_files():
 	for root, dirs, files in os.walk(os.path.start_path):
 		for dir in dirs:
 			if dir.endswith("dir"):
-				filelist.append(dir)
+				dirlist.append(dir)
 				print("\t" + str(os.path.join(root, dir)))
-	print("\n\t" + str(filelist))
+	print("\n\t" + str(dirlist))
 
 def get_start_path():
 	os.path.start_path = input("\tEnter the start path. Leave empty for test structure.\n\t")
 	if os.path.start_path == "":
 		os.path.start_path = "C:/Users/Schwizgebel/Programmieren"
 		print("\tUsing test path: " + str(os.path.start_path))
+
+def check_target():
+	print(".")
+
+def export_csv():
+	with open('targets.csv', 'w', newline='\n') as csvfile:
+		csvwriter = csv.writer(csvfile)
+		csvwriter.writerow(dirlist)
 	
 def main():
 	welcome_msg()
 	get_start_path()
 	search_files()
+	export_csv()
 
 main()
