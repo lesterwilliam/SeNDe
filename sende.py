01b03266fa99c77ec74bbf4708341675feb08f67
lesterwilliam <adrian.schwizgebel@gmail.com>
2018-12-12 15:39:39 +0100

    Updated sende.py. Working so far.

--- sende.py
+++ sende.py
@@ -5,22 +5,30 @@ import sys
 import os
 
 filelist = []
+os.path.start_path = 0
 
+def welcome_msg():
+	print("\nThis is -> SEARCH AND DESTROY <-\n")
 
 def search_files():
 	print("\nThe following directories have been found:\n")
-	for root, dirs, files in os.walk("C:/Users/Schwizgebel/Programmieren"):
+	#for root, dirs, files in os.walk("C:/Users/Schwizgebel/Programmieren"):
+	for root, dirs, files in os.walk(os.path.start_path):
 		for dir in dirs:
 			if dir.endswith("dir"):
 				filelist.append(dir)
-				print(os.path.join(root, dir))
-	print("\n" + str(filelist))
+				print("\t" + str(os.path.join(root, dir)))
+	print("\n\t" + str(filelist))
 
-#def get_start_path():
+def get_start_path():
+	os.path.start_path = input("\tEnter the start path. Leave empty for test structure.\n\t")
+	if os.path.start_path == "":
+		os.path.start_path = "C:/Users/Schwizgebel/Programmieren"
+		print("\tUsing test path: " + str(os.path.start_path))
 	
-
 def main():
+	welcome_msg()
+	get_start_path()
 	search_files()
 
-	
 main()
