8e5c4cdd3b4db2d0b38ce3784c2fe27e8db74726
lesterwilliam <adrian.schwizgebel@gmail.com>
2018-12-12 14:11:52 +0100

    Updated test structure

--- sende.py
+++ sende.py
@@ -1,17 +1,8 @@
 # Python code to search for the auto-created folders from the Renesas HEW IDE.
 # Once detected, choose the 'delete option' to delete all these folders.
 
-import os, fnmatch
-
-#dir_path = os.path.dirname(os.path.realpath(__file__))
-
-#for root, dirs, files in os.walk(dir_path):
-#	for file in files:
-#		if file.endswith('dir'):
-			#print root+'/'+str(file)
-
-def locate(pattern, root=os.curdir):
-    '''Locate all files matching supplied filename pattern in and below supplied root directory.'''
-    for path, dirs, files in os.walk(os.path.abspath(root)):
-        for filename in fnmatch.filter(files, pattern):
-            yield os.path.join(path, filename)
+import os
+for root, dirs, files in os.walk("C:/Users/Schwizgebel/Programmieren"):
+    for file in files:
+        if file.endswith(".txt"):
+             print(os.path.join(root, file))
