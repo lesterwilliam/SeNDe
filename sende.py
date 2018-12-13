e41583a3997d50190dfae70053a08a2c9fba44cb
lesterwilliam <adrian.schwizgebel@gmail.com>
2018-12-13 10:05:34 +0100

    Finished so far.

--- sende.py
+++ sende.py
@@ -5,6 +5,7 @@ import sys
 import os
 import csv
 from datetime import datetime
+import shutil
 
 folderlist = []
 dirlist = []
@@ -15,17 +16,28 @@ def welcome_msg():
 
 def get_time():
 	t = datetime.now()
-	return t.strftime('%d-%m-%y %H%M')
+	return t.strftime('%d-%m-%y %H-%M')
+
+def get_size(start_path = '.'):
+	total_size = 0
+	for dirpath, dirnames, filenames in os.walk(start_path):
+		for f in filenames:
+			fp = os.path.join(dirpath, f)
+			total_size += os.path.getsize(fp)
+	return total_size
 
 def search_files():
+	saved_space = 0
 	print("\nThe following directories have been found:\n")
 	for root, dirs, files in os.walk(os.path.start_path):
 		for dir in dirs:
 			if dir.endswith("dir"):
 				folderlist.append(dir)
 				dirlist.append(str(os.path.join(root, dir)))
+				saved_space += get_size(str(os.path.join(root, dir)))
 				print("\t" + str(os.path.join(root, dir)))
 	print("\n\t" + str(folderlist))
+	return saved_space
 
 def get_start_path():
 	os.path.start_path = input("\tEnter the start path. Leave empty for test structure.\n\t")
@@ -36,22 +48,29 @@ def get_start_path():
 def check_target():
 	print(".")
 
-def export_csv():
+def export_csv(input):
 	with open(str("logs/targets-" + str(get_time()) + ".csv"), 'w', newline='\n') as csvfile:
 		csvwriter = csv.writer(csvfile)
 		csvwriter.writerow("Directory: " + str(os.path.start_path))
 		csvwriter.writerow(folderlist)
 	
-def export_txt():
+def export_txt(input):
 	with open(str("logs/targets-" + str(get_time()) + ".txt"), "w") as dir_file:
+		dir_file.write("Freeable space: " + str(input) + " byte\n")
 		for item in dirlist:
 			dir_file.write("%s\n" % item)
+
+def delete():
+	if input("Do you want to delete all directories? (y/n):\n") == "y":
+		for item in dirlist:
+			shutil.rmtree(item, ignore_errors=True)
 
 def main():
 	welcome_msg()
 	get_start_path()
-	search_files()
-	export_csv()
-	export_txt()
+	availabel_space = search_files()
+	export_csv(availabel_space)
+	export_txt(availabel_space)
+	delete()
 
 main()
